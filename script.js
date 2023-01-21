aaf6f4c32ced3dbf0176a94e99b870e7ed0cc880
Reymer <117869795+reymerdela@users.noreply.github.com>
2023-01-20 22:46:14 -0400

    Add playRound() function and loop this with game() function

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+
+
 function getComputerChoice () {
-    let piedras = "piedras";
+    let piedras = "piedra";
     let papel = "papel";
     let tijeras = "tijeras";
     let resultado;
@@ -14,9 +16,39 @@ function getComputerChoice () {
         resultado = papel;
     }
     else {
-        resultado = tijeras
+        resultado = tijeras;
     }
     return resultado;
 }
 
-console.log(getComputerChoice());
+
+
+
+function playRound () {
+  let playerSelection = prompt("Piedra, Papel o Tijeras!").toLowerCase();
+    let a = playerSelection;
+    let b = getComputerChoice();
+    let result;
+    
+    if (a == b) {
+        result = "Empate!";
+    }
+    else if (a == "piedra" && b == "papel"|| a == "papel" && b == "tijeras"|| a == "tijeras" && b == "piedra") {
+        result = "Perdiste! " + b + " gana a "+ a;
+        
+    }
+    else if (a == "piedra" && b == "tijeras"|| a == "papel" && b == "piedra"|| a == "tijeras" && b == "papel") {
+        result = "Ganaste " + a +" gana a " + b;
+    }
+    return result;
+}
+
+function game () {
+
+    for(let i = 0; i < 5; i++) {
+       alert(playRound());      
+    }
+}
+
+game();
+
